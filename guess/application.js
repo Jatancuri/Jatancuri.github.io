5229eb1f46886dc8e5f7ffb27862581e45f0b8d8
Jatancuri <jatancuri@kippnyccp.org>
2017-06-02 13:14:39 -0400

    Create application.js

---
+++ guess/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var random_num = Math.floor((Math.random()*100) + 1);
+	//create click event for submit button
+  $("Sumbit").click(function(){
+    
+
+
+	//create click event for clear button
+
+
+});
