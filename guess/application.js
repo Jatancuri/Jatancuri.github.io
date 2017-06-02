f7ab4049f0fe9a67f70a9533cfec96ed89cb8ff6
Jatancuri <jatancuri@kippnyccp.org>
2017-06-02 13:16:12 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
-  $("Sumbit").click(function(){
-    
+	$("Sumbit").click(function(){
+		$("#Submit").css("color","red");
 
 
 	//create click event for clear button
